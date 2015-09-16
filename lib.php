9b90066baed1f1c2b77bba39f0295f0d8e5c0084
Edwin Phillips <edwin.phillips@catalyst-eu.net>
2015-09-16 13:10:25 +0100

    Initial commit (1.9)

---
+++ lib.php
@@ -0,0 +1,60 @@
+<?php  // $Id$
+
+/**
+ * Library of functions and constants for module biblogin
+ * This file should have two well differenced parts:
+ *   - All the core Moodle functions, neeeded to allow
+ *     the module to work integrated in Moodle.
+ *   - All the biblogin specific functions, needed
+ *     to implement all the module logic. Please, note
+ *     that, if the module become complex and this lib
+ *     grows a lot, it's HIGHLY recommended to move all
+ *     these module specific functions to a new php file,
+ *     called "locallib.php" (see forum, quiz...). This will
+ *     help to save some memory when Moodle is performing
+ *     actions across all modules.
+ */
+
+
+/**
+ * Function to be run periodically according to the moodle cron
+ * This function searches for things that need to be done, such
+ * as sending out mail, toggling flags etc ...
+ *
+ * @return boolean
+ * @todo Finish documenting this function
+ **/
+function webproxy_cron() {
+	
+	return true;
+}
+
+
+/**
+ * Execute post-install custom actions for the module
+ * This function was added in 1.9
+ *
+ * @return boolean true if success, false on error
+ */
+function webproxy_install() {
+	return true;
+}
+
+/**
+ * Execute post-uninstall custom actions for the module
+ * This function was added in 1.9
+ *
+ * @return boolean true if success, false on error
+ */
+function webproxy_uninstall() {
+	return true;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+/// Any other webproxy functions go here.  Each of them must have a name that
+/// starts with biblogin_
+/// Remember (see note in first lines) that, if this section grows, it's HIGHLY
+/// recommended to move all funcions below to a new "localib.php" file.
+
+
+?>
